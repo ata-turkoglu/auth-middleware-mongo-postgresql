9bcf1f544a6e9afed5ceba95648b6987638216b8
ata <ataturkoglu@outlook.com.tr>
2021-11-29 22:46:13 +0300

    :bug: fix: add log registry to isAdmin middleware

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,12 +1,25 @@
 const User = require("../models/user")
+const Log = require("../models/log");
 
 module.exports= async(req,res,next)=>{
-  let id = req.user.id
-  if(!id) return res.status(401).json({message:"unauthorized id"})
   try {
-    const user = await User.findById(id)
-    if(user.role!=="admin") return res.status(401).json({message:"You are not admin"})
-    next()
+    const user = await User.findById(req.user.id)
+    if (user.role=="admin") {
+      next();
+    } else {
+      let log = new Log({
+        hostname: req.headers.host,
+        url: req.originalUrl,
+        userId: id,
+        username: user.username,
+        email: user.email,
+        age: user.age,
+        role: user.role,
+        type: "invalid",
+      });
+      await log.save();
+      return res.status(401).json({ message: "Unauthorized request" });
+    }
   } catch (error) {
     res.status(500).json({message:"Invalid token"})
   }
